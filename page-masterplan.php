2b5fbd256e5c6ba5012e8592f2fbccc6bf10c47a
Tran Danh Trong <codevnes@gmail.com>
2025-06-17 13:43:08 +0700

    Update Tue Jun 17 13:43:08 +07 2025

---
+++ page-masterplan.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Template Name: Trang Mặt Bằng
+ *
+ * @package QP3
+ */
+get_header();
+?>
+
+<div id="fullpage">
+    <ul id="menu">
+        <li data-menuanchor="anchor-contact"><a href="#anchor-contact"><span class="menu-nb">07</span><span
+                    class="menu-tt"> Liên hệ </span></a></li>
+    </ul>
+
+    <?php get_template_part('template-parts/home/section', 'contact'); ?>
+</div>
+<script>
+    jQuery(document).ready(function () {
+        if (jQuery(window).width() > 1089) {
+            var myFullpage = new fullpage('#fullpage', {
+                verticalCentered: true,
+                anchors: ['anchor-contact'],
+                lazyLoad: true,
+                // licenseKey: 'OPEN-SOURCE-GPLV3-LICENSE',
+                lockAnchors: false,
+                slidesNavigation: false,
+                controlArrows: false,
+                normalScrollElements: '.mobilenav__inner',
+                css3: true,
+                navigation: false,
+                menu: '#menu'
+            });
+        }
+
+    });
+</script>
+<?php get_footer(); ?>
